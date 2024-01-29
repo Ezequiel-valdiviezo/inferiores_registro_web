4c23eb0fe9cee80481442b364f68bb4776d5927d
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-01-29 11:11:00 -0300

    Footer casi finalizado

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import Logo from '../assets/logo.jpeg'
 
 function Footer(){
 
     return(
-        <footer>
-            <h1>Hola</h1>
+        <footer className="bg-black d-flex justify-content-between align-items-center p-4">
+            <div>
+                <img width='70%' src={Logo} alt="Logo Gintita" 
+                className=""/>
+            </div>
+            <div className="text-white">
+                <h2>Contacto</h2>
+                <p>+54 9 11 5445 5454</p>
+                <p>danielcabrera@gmail.com</p>
+            </div>
+            <div className="text-white">
+                <h2>Redes Sociales</h2>
+                <p>Instagram</p>
+                <p>Facebook</p>
+                <p>Youtube</p>
+            </div>
         </footer>
     )
 }
