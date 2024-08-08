52a996efee2ddf9bc4fd3ce7eea11279df657dcb
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-08-08 13:41:53 -0300

    Diseño de las card de novedades

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -28,11 +28,11 @@ function App() {
       <div className="seccion2 row text-center mt-5" id='nosotros'>
         <div className='col-md-6'>
           <h2 className='tituloh2'>Gintita</h2>
-          <div className='mt-5 descripcionGintita p-2'>
-            <p>Somos un centro de capacitación y formación de jugadores. Los entrenamos y preparamos para salida a AFA y mejorar su rendimiento.</p>
-            <p>Creado por Daniel Cabrera, director técnico y coaching deportivo, el 18 de octubre del 2020</p>
+          <div className='mt-5 descripcionGintita px-5'>
+            <p className='fw-bold'>Somos un centro de capacitación y formación de jugadores. Los entrenamos y preparamos para salida a AFA y mejorar su rendimiento.</p>
+            <p className='fw-bold'>Creado por Daniel Cabrera, director técnico y coaching deportivo, el 18 de octubre del 2020</p>
             <button>Contacto</button>
-            <p className='text-danger'>*Los entrenamientos se realizan por la mañana</p>
+            <p className='text-danger fw-bold mt-2'>*Los entrenamientos se realizan por la mañana</p>
           </div>
         </div>
         <div className='col-md-6'>
@@ -41,7 +41,7 @@ function App() {
             title="Google Maps"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62444.380248832225!2d-58.50324655800285!3d-34.65903463895763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccbd84981b36d%3A0x2b0d85df553a6d5a!2sClub%20Social%20Y%20Deportivo%20Pintita!5e0!3m2!1ses-419!2sar!4v1706491996812!5m2!1ses-419!2sar"
             width="85%"
-            height="300"
+            height="280"
             style={{ border: '0' }}
             allowFullScreen=""
             loading="lazy"
--- vite-project/src/components/Novedades.jsx
+++ vite-project/src/components/Novedades.jsx
@@ -6,60 +6,59 @@ function Novedades(){
     return(
         <div className="text-center" id="novedades">
         <h2 className='tituloh2'>Novedades</h2>
-        <div className="d-flex flex-wrap p-5">
-            <div className="col-12 col-lg-6 ">
-                <div className="card m-2">
-                    <div className="d-flex">
-                        <div>
-                            <img width={250} src={Pintita} alt="" />
-                        </div>
-                        <div>
-                            <h3>Noticias</h3>
-                            <p>Prueba de jugador categorías 2002</p>
-                        </div>
-                    </div>
+
+        <div className="d-flex flex-wrap justify-content-center">
+            <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+                <img src={Pintita} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Noticias</h5>
+                    <a href="#" className="btn btn-primary">Leer más</a>
                 </div>
             </div>
-            <div className="col-12 col-lg-6 ">
-                <div className="card m-2">
-                    <div className="d-flex">
-                        <div>
-                            <img width={250} src={Pintita} alt="" />
-                        </div>
-                        <div>
-                            <h3>Noticias</h3>
-                            <p>Prueba de jugador categorías 2002</p>
-                        </div>
-                    </div>
+            <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+                <img src={Pintita} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Noticias Noticias Noticias Noticias Noticias Noticias NoticiasNoticias</h5>
+                    <a href="#" className="btn btn-primary">Leer más</a>
                 </div>
             </div>
-            <div className="col-12 col-lg-6 ">
-                <div className="card m-2">
-                    <div className="d-flex">
-                        <div>
-                            <img width={250} src={Pintita} alt="" />
-                        </div>
-                        <div>
-                            <h3>Noticias</h3>
-                            <p>Prueba de jugador categorías 2002</p>
-                        </div>
-                    </div>
+            <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+                <img src={Pintita} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Noticias</h5>
+                    <a href="#" className="btn btn-primary">Leer más</a>
                 </div>
             </div>
-            <div className="col-12 col-lg-6 ">
-                <div className="card m-2">
-                    <div className="d-flex">
-                        <div>
-                            <img width={250} src={Pintita} alt="" />
-                        </div>
-                        <div>
-                            <h3>Noticias</h3>
-                            <p>Prueba de jugador categorías 2002</p>
-                        </div>
-                    </div>
+            <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+                <img src={Pintita} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Noticias</h5>
+                    <a href="#" className="btn btn-primary">Leer más</a>
                 </div>
-            </div> 
+            </div>
+            <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+                <img src={Pintita} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Noticias</h5>
+                    <a href="#" className="btn btn-primary">Leer más</a>
+                </div>
+            </div>
+            <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+                <img src={Pintita} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Noticias</h5>
+                    <a href="#" className="btn btn-primary">Leer más</a>
+                </div>
+            </div>
+            <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+                <img src={Pintita} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Noticias</h5>
+                    <a href="#" className="btn btn-primary">Leer más</a>
+                </div>
+            </div>
         </div>
+
         </div>
     )
 }
