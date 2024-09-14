cc7fb278bb3d8ad071e6aee73943f61984c30798
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-09-14 18:23:18 -0300

    Arreglo error de estilos

--- back/admin/noticiasAdmin.php
+++ back/admin/noticiasAdmin.php
@@ -27,7 +27,7 @@ if ($result === false) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Administración de Noticias</title>
-    <link rel="stylesheet" href="./estilos/home.css">
+    <link rel="stylesheet" href="../estilos/home.css">
 </head>
 <body>
     <div class="container text-center pt-3">
